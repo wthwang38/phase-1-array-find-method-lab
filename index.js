6faa33665c3c35527e19b59cc9a0fb74cc2b07e7
William Hwang <willhwang90@gmail.com>
2022-04-28 09:17:03 -0400

    commit tree 8dbdac59a2e998b98d822d87f5b10733aedd97f0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+const superbowlWin = (record) => {
+    const result = record.find((record) => record.result === "W");
+    return result ? result.year : undefined;
+}
+
+/*const superbowlWin = (record) => {
+    record.find((record) => {
+    (record.result === "W") ? record.year : undefined;
+    })
+}*/
+
+//i needed a variable to return
